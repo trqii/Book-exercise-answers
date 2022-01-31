f0323ff6e77078607128fdcc19cd4797e690298e
trqii <3038605486@qq.com>
2022-02-01 00:05:33 +0800

    1-1

---
+++ 明解C语言中级篇/chapter01/1-1.c
@@ -0,0 +1,28 @@
+﻿#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int ans;
+
+	srand(time(NULL));
+	ans = rand() % 7;
+
+	if (ans == 0)
+		printf("大吉\n");
+	else if (ans == 1)
+		printf("中吉\n");
+	else if (ans == 2)
+		printf("小吉\n");
+	else if (ans == 3)
+		printf("吉\n");
+	else if (ans == 4)
+		printf("末吉\n");
+	else if (ans == 5)
+		printf("凶\n");
+	else if (ans == 6)
+		printf("大凶\n");
+
+	return 0;
+}
