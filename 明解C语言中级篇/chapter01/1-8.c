ba130548f1631e7709086a55bb3fba5fd187b8ae
trqii <3038605486@qq.com>
2022-02-07 00:04:22 +0800

    1-8

---
+++ 明解C语言中级篇/chapter01/1-8.c
@@ -0,0 +1,43 @@
+﻿#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STAGE 10
+
+int main(void)
+{
+	int i;
+	int stage;
+	int no = -1;
+	int ans;
+	int num[MAX_STAGE];
+
+	srand(time(NULL));
+	ans = rand() % 1000;
+
+	printf("请猜一个0~999的整数。\n\n");
+
+	for (stage = 0; no != ans && stage < MAX_STAGE; stage++) {
+		printf("还剩%d次机会。是多少呢：", MAX_STAGE - stage);
+		scanf("%d", &no);
+		num[stage] = no;
+
+		if (no > ans)
+			printf("\a再小一点。\n");
+		else if (no < ans)
+			printf("\a再大一点。\n");
+	}
+
+	if (no != ans)
+		printf("\a很遗憾，正确答案是%d。\n", ans);
+	else {
+		printf("回答正确。\n");
+		printf("您用了%d次猜中了。\n", stage);
+	}
+
+	puts("\n--- 输入记录 ---");
+	for (i = 0; i < stage; i++)
+		printf(" %2d : %4d %+4d\n", i + 1, num[i], num[i] - ans);
+
+	return 0;
+}
