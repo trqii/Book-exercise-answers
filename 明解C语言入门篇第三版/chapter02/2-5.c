bc19c041574a532a20d973435409145d5e3ff1fc
trqii <3038605486@qq.com>
2022-02-09 11:59:00 +0800

    2-5

---
+++ 明解C语言入门篇第三版/chapter02/2-5.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	puts("请输入两个整数。");
+	printf("整数a："); scanf("%d", &a);
+	printf("整数b："); scanf("%d", &b);
+
+	printf("a是b的%f%%。\n", 100.0 * a / b);
+
+	return 0;
+}
