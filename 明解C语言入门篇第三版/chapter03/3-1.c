882cf96352d41fdc2ecdcdc320e8f1b344e02ed0
trqii <3038605486@qq.com>
2022-02-12 09:14:02 +0800

    3-1

---
+++ 明解C语言入门篇第三版/chapter03/3-1.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	puts("请输入两个整数。");
+	printf("整数A："); scanf("%d", &A);
+	printf("整数B："); scanf("%d", &B);
+
+	if (A % B)
+		puts("B不是A的约数。");
+	else
+		puts("B是A的约数。");
+
+	return 0;
+}
