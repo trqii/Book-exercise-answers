0699ee9fa86cb203408f6c8ad35fadc61c76111a
trqii <3038605486@qq.com>
2022-02-14 16:15:20 +0800

    3-5

---
+++ 明解C语言入门篇第三版/chapter03/3-5.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", 1 > 2);
+	printf("%d\n", 2 > 1);
+
+	printf("%d\n", 1 < 2);
+	printf("%d\n", 2 < 1);
+
+	printf("%d\n", 1 == 2);
+	printf("%d\n", 2 == 1);
+	printf("%d\n", 1 == 1);
+	printf("%d\n", 2 == 2);
+
+	return 0;
+}
