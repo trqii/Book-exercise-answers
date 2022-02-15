cfd52cdac472163d1a02d12fff26af6ec7220a3e
trqii <3038605486@qq.com>
2022-02-15 11:50:48 +0800

    3-10

---
+++ 明解C语言入门篇第三版/chapter03/3-10.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int A, B, C;
+
+	puts("请输入三个整数。");
+	printf("整数A："); scanf("%d", &A);
+	printf("整数B："); scanf("%d", &B);
+	printf("整数C："); scanf("%d", &C);
+
+	if (A == B && B == C)
+		puts("三个值都相等。");
+	else if (A == B || A == C || B == C)
+		puts("有两个值相等。");
+	else
+		puts("三个值各不相同。");
+
+	return 0;
+}
