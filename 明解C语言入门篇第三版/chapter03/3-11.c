48bc4edda7fe1f3ba921fb64e539987d417f47ff
trqii <3038605486@qq.com>
2022-02-16 10:28:59 +0800

    3-11

---
+++ 明解C语言入门篇第三版/chapter03/3-11.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	puts("请输入两个整数。");
+	printf("整数A："); scanf("%d", &A);
+	printf("整数B："); scanf("%d", &B);
+
+	if ((A > B && A - B <= 10) || (A < B && A - B >= -10))
+		puts("它们的差小于等于 10。");
+	else
+		puts("它们的差大于等于 11。");
+
+	return 0;
+}
