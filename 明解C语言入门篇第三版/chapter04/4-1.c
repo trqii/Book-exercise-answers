2e2af543e9c70dc972bcd99c673be74143d50023
trqii <3038605486@qq.com>
2022-02-17 14:06:08 +0800

    4-1

---
+++ 明解C语言入门篇第三版/chapter04/4-1.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int retry;
+
+	do {
+		int no;
+		printf("请输入一个整数：");
+		scanf("%d", &no);
+
+		if (no == 0)
+			puts("该整数等于0。");
+		else if (no > 0)
+			puts("该整数为正数。");
+		else
+			puts("该整数为负数。");
+
+		printf("要重复一次吗？【Yes…0 / No…9】：");
+		scanf("%d", &retry);
+	} while (retry == 0);
+
+	return 0;
+}
