484eef4eb42f938ed692dc1efd69e9ab57a1fc04
trqii <3038605486@qq.com>
2022-02-19 20:21:55 +0800

    4-3

---
+++ 明解C语言入门篇第三版/chapter04/4-3.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int no, sign;
+
+	printf("请输入一个正整数：");
+	scanf("%d", &no);
+
+	sign = no;
+	while (no >= 0) {
+		printf("%d ", no);
+		no--;
+	}
+	if (sign >= 0)
+		printf("\n");
+
+	return 0;
+}
