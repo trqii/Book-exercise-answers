6c1e851dd3b2f5d8e12bc3ac748108e3d1d2581e
trqii <3038605486@qq.com>
2022-02-22 22:01:30 +0800

    4-9

---
+++ 明解C语言入门篇第三版/chapter04/4-9.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i = 1, no;
+
+	printf("正整数：");
+	scanf("%d", &no);
+
+	while (i <= no) {
+		if (i % 2)
+			printf("+");
+		else
+			printf("-");
+		i++;
+	}
+
+	return 0;
+}
