9d69e36534cfeeab862a642aedab8c0f40b9bc78
trqii <3038605486@qq.com>
2022-02-24 12:27:22 +0800

    4-12

---
+++ 明解C语言入门篇第三版/chapter04/4-12.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int no;
+	int count = 0;
+
+	do {
+		printf("请输入一个正整数：");
+		scanf("%d", &no);
+		if (no <= 0)
+			puts("\a 请不要输入非正整数。");
+	} while (no <= 0);
+
+	printf("%d的位数是", no);
+	while (no > 0) {
+		count++;
+		no /= 10;
+	}
+	printf("%d。\n", count);
+
+	return 0;
+}
