a1da4f46be1f6888476720cb66d8745511e29d37
trqii <3038605486@qq.com>
2022-02-28 21:30:43 +0800

    4-18

---
+++ 明解C语言入门篇第三版/chapter04/4-18.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i, n;
+
+	printf("显示多少个*：");
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		printf("*");
+		if (i % 5 == 0)
+			printf("\n");
+	}
+
+	return 0;
+}
