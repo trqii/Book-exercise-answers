bc72883fa6456f4683da6af53eccbc65a86e5e58
trqii <3038605486@qq.com>
2022-02-28 21:32:13 +0800

    4-21

---
+++ 明解C语言入门篇第三版/chapter04/4-21.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i, j, n;
+
+	puts("生成一个正方形");
+	printf("正方形有几层：");
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
