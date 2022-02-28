c0d681915bf1bbacac0b84aaf1b4df53e90a2a02
trqii <3038605486@qq.com>
2022-02-28 21:31:45 +0800

    4-20

---
+++ 明解C语言入门篇第三版/chapter04/4-20.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+
+	printf("  |");
+	for (i = 1; i <= 9; i++)
+		printf("%3d", i);
+	printf("\n");
+
+	for (i = 0; i <= 9; i++)
+		printf("---");
+	printf("\n");
+
+	for (i = 1; i <= 9; i++) {
+		printf("%d |", i);
+		for (j = 1; j <= 9; j++)
+			printf("%3d", i * j);
+		printf("\n");
+	}
+
+	return 0;
+}
