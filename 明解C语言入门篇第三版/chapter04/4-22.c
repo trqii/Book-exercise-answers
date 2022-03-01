d204ddfa2cf79fdb03d1f5ddd035a6729889c030
trqii <3038605486@qq.com>
2022-03-01 21:09:15 +0800

    4-22

---
+++ 明解C语言入门篇第三版/chapter04/4-22.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i, j, n1, n2;
+
+	puts("让我们来画一个长方形。");
+	printf("一边：");   scanf("%d", &n1);
+	printf("另一边："); scanf("%d", &n2);
+
+	if (n1 > n2) {
+		int tmp = n1;
+		n1 = n2;
+		n2 = tmp;
+	}
+
+	for (i = 1; i <= n1; i++) {
+		for (j = 1; j <= n2; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
