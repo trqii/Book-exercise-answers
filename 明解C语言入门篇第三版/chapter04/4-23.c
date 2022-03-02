f4c08260b322515693ed709ac497317ae8988644
trqii <3038605486@qq.com>
2022-03-02 11:17:33 +0800

    4-23

---
+++ 明解C语言入门篇第三版/chapter04/4-23.c
@@ -0,0 +1,39 @@
+﻿//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i, j, len;
+//
+//	puts("生成直角在左上方的等腰直角三角形。");
+//	printf("短边：");
+//	scanf("%d", &len);
+//
+//	for (i = len; i >= 1; i--) {
+//		for (j = 1; j <= i; j++)
+//			printf("*");
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, len;
+
+	puts("生成直角在右上方的等腰直角三角形。");
+	printf("短边：");
+	scanf("%d", &len);
+
+	for (i = len; i >= 1; i--) {
+		for (j = 1; j <= len - i; j++)
+			printf(" ");
+		for (j = 1; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
