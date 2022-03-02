3d01d5cc8e04d8c52ac6c5264c59c5868dab5cd0
trqii <3038605486@qq.com>
2022-03-02 11:18:03 +0800

    4-24

---
+++ 明解C语言入门篇第三版/chapter04/4-24.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i, j, len;
+
+	puts("让我们来画一个金字塔。");
+	printf("金字塔有几层：");
+	scanf("%d", &len);
+
+	for (i = 1; i <= len; i++) {
+		for (j = 1; j <= len - i; j++)
+			printf(" ");
+		for (j = 1; j <= 2 * i - 1; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
