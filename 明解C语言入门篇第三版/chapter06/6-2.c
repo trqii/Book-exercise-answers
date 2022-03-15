ef7889ce1f328301d7b88e9f29003085697fb15e
trqii <3038605486@qq.com>
2022-03-15 12:40:19 +0800

    6-2

---
+++ 明解C语言入门篇第三版/chapter06/6-2.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int min3(int a, int b, int c)
+{
+	int min = a;
+
+	if (b < min) min = b;
+	if (c < min) min = c;
+	return min;
+}
+
+int main(void)
+{
+	int a, b, c;
+
+	puts("请输入三个整数。");
+	printf("整数 a："); scanf("%d", &a);
+	printf("整数 b："); scanf("%d", &b);
+	printf("整数 c："); scanf("%d", &c);
+
+	printf("最小值是 %d。\n", min3(a, b, c));
+
+	return 0;
+}
