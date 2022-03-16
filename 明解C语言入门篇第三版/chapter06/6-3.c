78d19a295c9dc308933eed1b5593bff59a556d4b
trqii <3038605486@qq.com>
2022-03-16 15:54:24 +0800

    6-3

---
+++ 明解C语言入门篇第三版/chapter06/6-3.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int cube(int x)
+{
+	return x * x * x;
+}
+
+int main(void)
+{
+	int n1;
+
+	printf("请输入一个整数：");
+	scanf("%d", &n1);
+
+	printf("%d的立方是：%d。\n", n1, cube(n1));
+
+	return 0;
+}
