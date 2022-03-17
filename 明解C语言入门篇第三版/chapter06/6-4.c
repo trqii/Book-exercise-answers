f09ad3e02dc410d0d3a9dceaec4247ef4b3d6be0
trqii <3038605486@qq.com>
2022-03-17 11:15:39 +0800

    6-4

---
+++ 明解C语言入门篇第三版/chapter06/6-4.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int sqr(int x)
+{
+	return x * x;
+}
+
+int pow4(int x)
+{
+	return sqr(x) * sqr(x);
+}
+
+int main(void)
+{
+	int n1;
+
+	printf("请输入一个整数：");
+	scanf("%d", &n1);
+
+	printf("%d的四次幂是：%d。\n", n1, pow4(n1));
+
+	return 0;
+}
