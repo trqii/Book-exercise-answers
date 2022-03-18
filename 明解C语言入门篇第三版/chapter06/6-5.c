ffc1e6d55e826c48f444b5b7a89e22156f0e397a
trqii <3038605486@qq.com>
2022-03-18 20:34:20 +0800

    6-5

---
+++ 明解C语言入门篇第三版/chapter06/6-5.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int sumup(int n)
+{
+	int sum = 0;
+	while (n) {
+		sum += n;
+		n--;
+	}
+	return sum;
+}
+
+int main(void)
+{
+	int n1;
+
+	printf("请输入一个整数：");
+	scanf("%d", &n1);
+
+	printf("1到%d之间所有的整数和是：%d。\n", n1, sumup(n1));
+
+	return 0;
+}
