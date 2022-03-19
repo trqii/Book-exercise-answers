f03d5aaaa1d608220764276cf019478a6152da09
trqii <3038605486@qq.com>
2022-03-19 10:07:12 +0800

    6-6

---
+++ 明解C语言入门篇第三版/chapter06/6-6.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+void alert(int n)
+{
+	while (n--) {
+		printf("\a");
+	}
+}
+
+int main(void)
+{
+	int n1;
+
+	printf("请输入一个整数：");
+	scanf("%d", &n1);
+
+	alert(n1);
+
+	return 0;
+}
