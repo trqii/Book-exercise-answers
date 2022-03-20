e645b2e481ab467aa5a99c94c22534b371f1d04b
trqii <3038605486@qq.com>
2022-03-21 00:42:01 +0800

    6-8

---
+++ 明解C语言入门篇第三版/chapter06/6-8.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int min_of(const int v[], int n)
+{
+	int i;
+	int min = v[0];
+
+	for (i = 1; i < n; i++)
+		if (min > v[i])
+			min = v[i];
+
+	return min;
+}
+
+int main(void)
+{
+	int arr[5] = { 1, 2, 3, 4, 5 };
+
+	printf("数组中最小值为：%d。\n", min_of(arr, 5));
+
+	return 0;
+}
