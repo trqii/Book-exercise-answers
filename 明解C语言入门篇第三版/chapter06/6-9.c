d3dbc4b0d6feaea63a8d3f020b18e124fd43164c
trqii <3038605486@qq.com>
2022-03-22 00:01:56 +0800

    6-9

---
+++ 明解C语言入门篇第三版/chapter06/6-9.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+void rev_intary(int v[], int n)
+{
+	int i;
+	for (i = 0; i < n / 2; i++) {
+		int tmp = v[i];
+		v[i] = v[n - i - 1];
+		v[n - i - 1] = tmp;
+	}
+}
+
+int main(void)
+{
+	int arr[5] = { 1, 2, 3, 4, 5 };
+	int i;
+
+	puts("数组倒序存储。");
+
+	rev_intary(arr, 5);
+
+	for (i = 0; i < 5; i++)
+		printf("%d ", arr[i]);
+	putchar('\n');
+
+	return 0;
+}
