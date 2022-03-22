0d5ca385f94ffb234aa08372254b92475c2d3ea5
trqii <3038605486@qq.com>
2022-03-23 00:03:00 +0800

    6-10

---
+++ 明解C语言入门篇第三版/chapter06/6-10.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+void intary_rcpy(int v1[], const int v2[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		v1[i] = v2[n - i - 1];
+}
+
+int main(void)
+{
+	int arr1[5] = { 0 };
+	int arr2[5] = { 1, 2, 3, 4, 5 };
+	int i;
+
+	puts("v2倒序排列。");
+
+	intary_rcpy(arr1, arr2, 5);
+
+	for (i = 0; i < 5; i++)
+		printf("%d ", arr1[i]);
+	putchar('\n');
+
+	return 0;
+}
