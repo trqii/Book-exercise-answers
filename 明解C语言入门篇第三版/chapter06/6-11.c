cb01aa8bcc3c9bf053b21f15fc6663b556caca9f
trqii <3038605486@qq.com>
2022-03-23 00:15:29 +0800

    6-11

---
+++ 明解C语言入门篇第三版/chapter06/6-11.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+#define NUMBER 7
+
+int search_idx(const int v[], int idx[], int key, int n)
+{
+	int i, j = 0;
+	for (i = 0; i < n; i++)
+		if (v[i] == key)
+			idx[j++] = i;
+	return j;
+}
+
+int main(void)
+{
+	int v[NUMBER] = { 1, 7, 5, 7, 2, 4, 7 };
+	int idx[NUMBER] = { 0 };
+	int key = 7;
+	int i;
+
+	int n = search_idx(v, idx, key, NUMBER);
+
+	for (i = 0; i < n; i++)
+		printf("%d ", idx[i]);
+	putchar('\n');
+
+	return 0;
+}
