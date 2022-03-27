22e72875bdd062ee7a9e69ad7f05e806c8e212b9
trqii <3038605486@qq.com>
2022-03-28 00:37:41 +0800

    7-1

---
+++ 明解C语言入门篇第三版/chapter07/7-1.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	unsigned n = 0;
+
+	printf("%u\n", sizeof 1);
+	printf("%u\n", sizeof + 1);
+	printf("%u\n", sizeof - 1);
+
+	printf("%u\n", sizeof(unsigned) - 1);
+	printf("%u\n", sizeof(double) - 1);
+	printf("%u\n", sizeof((double)-1));
+
+	printf("%u\n", sizeof n + 2);
+	printf("%u\n", sizeof(n + 2));
+	printf("%u\n", sizeof(n + 2.0));
+
+	return 0;
+}
