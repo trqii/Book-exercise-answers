6271a62cfbae04918712e10fbd99186650b0f120
trqii <3038605486@qq.com>
2022-03-27 18:35:33 +0800

    6-15

---
+++ 明解C语言入门篇第三版/chapter06/6-15.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+void put_count()
+{
+	static count = 1;
+	printf("第%d次\n", count++);
+}
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 3; i++) {
+		printf("put_count: ");
+		put_count();
+	}
+
+	return 0;
+}
