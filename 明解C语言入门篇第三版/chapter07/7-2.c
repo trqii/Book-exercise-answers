16f0745d70a67c93f0ea5ce397e79f6f08ef28cc
trqii <3038605486@qq.com>
2022-03-29 00:30:18 +0800

    7-2

---
+++ 明解C语言入门篇第三版/chapter07/7-2.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+unsigned rrotate(unsigned x, int n)
+{
+	return (x >> n) == (unsigned)(x / pow(2, n));
+}
+
+unsigned lrotate(unsigned x, int n)
+{
+	return (x << n) == (unsigned)(x * pow(2, n));
+}
+
+int main(void)
+{
+	unsigned x, n;
+
+	printf("非负整数："); scanf("%u", &x);
+	printf("位移位数："); scanf("%u", &n);
+
+	printf("左移%d位是否等于其乘以2的指数幂：%u\n", n, lrotate(x, n));
+	printf("右移%d位是否等于其除以2的指数幂：%u\n", n, rrotate(x, n));
+
+	return 0;
+}
