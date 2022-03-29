0d819256eddad5bc5711c923a3ecd54d687e1342
trqii <3038605486@qq.com>
2022-03-30 00:06:43 +0800

    7-3

---
+++ 明解C语言入门篇第三版/chapter07/7-3.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+unsigned rrotate(unsigned x, int n)
+{
+	return (x >> n);
+}
+
+unsigned lrotate(unsigned x, int n)
+{
+	return (x << n);
+}
+
+int main(void)
+{
+	unsigned x, n;
+
+	printf("非负整数："); scanf("%u", &x);
+	printf("位移位数："); scanf("%u", &n);
+
+	printf("左移%d位：%u\n", n, lrotate(x, n));
+	printf("右移%d位：%u\n", n, rrotate(x, n));
+
+	return 0;
+}
