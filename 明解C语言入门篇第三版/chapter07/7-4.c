d14586a5a9ae329cf370bbe29b3fd9e527b42bce
trqii <3038605486@qq.com>
2022-03-31 00:09:52 +0800

    7-4

---
+++ 明解C语言入门篇第三版/chapter07/7-4.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+unsigned set(unsigned x, int pos)
+{
+	return (x | (1U << (sizeof(int) * 8 - pos)));
+}
+
+unsigned inverse(unsigned x, int pos)
+{
+	return (x ^ (set(0U, pos)));
+}
+
+unsigned reset(unsigned x, int pos)
+{
+	return (x & (inverse(~0U, pos)));
+}
+
+int main(void)
+{
+	unsigned x, n;
+
+	printf("非负整数："); scanf("%u", &x);
+	printf("改变位数："); scanf("%u", &n);
+
+	printf("第%d位设置为1后的值%u。\n", n, set(x, n));
+	printf("第%d位设置为0后的值%u。\n", n, reset(x, n));
+	printf("第%d位设置取反后的值%u。\n", n, inverse(x, n));
+
+	return 0;
+}
