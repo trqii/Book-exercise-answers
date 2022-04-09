4c527a176c394425bfe35eeebd0a296c1b1f8e04
trqii <3038605486@qq.com>
2022-04-09 23:15:51 +0800

    7-11

---
+++ 明解C语言入门篇第三版/chapter07/7-11.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	float x;
+	int i = 0;
+	double sum1 = 0, sum2 = 0;
+
+	for (x = 0.0; x <= 1.0; x += 0.01)
+		sum1 += x, sum2 += i++ / 100.0;
+
+	printf("%f %f\n", sum1, sum2);
+
+	return 0;
+}
