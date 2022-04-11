14a5412aa26b070f447561bc0ddb4ee4ca1c915a
trqii <3038605486@qq.com>
2022-04-11 21:14:55 +0800

    8-1

---
+++ 明解C语言入门篇第三版/chapter08/8-1.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+#define diff(x, y) (((x) > (y)) ? (x) - (y) : (y) - (x))
+
+int main(void)
+{
+	int n1, n2;
+
+	puts("请输入两个数：");
+
+	printf("第一个："); scanf("%d", &n1);
+	printf("第二个："); scanf("%d", &n2);
+
+	printf("二值之差：%d\n", diff(n1, n2));
+
+	return 0;
+}
