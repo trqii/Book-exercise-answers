4a5010c07b4dc74c5d596272ab9cbd2cfd4b5173
trqii <3038605486@qq.com>
2022-04-15 22:01:58 +0800

    8-3

---
+++ 明解C语言入门篇第三版/chapter08/8-3.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+#define swap(type, a, b) { type tmp = a; a = b; b = tmp; }
+
+int main(void)
+{
+	int a = 5;
+	int b = 10;
+
+	swap(int, a, b);
+
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+
+	return 0;
+}
