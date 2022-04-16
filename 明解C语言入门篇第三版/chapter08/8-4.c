8d0a25df6c00feabebf2bf9be26c747d4f366427
trqii <3038605486@qq.com>
2022-04-16 20:55:22 +0800

    8-4

---
+++ 明解C语言入门篇第三版/chapter08/8-4.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+
+#define NUMBER 5
+
+void bsort(int a[], int n)
+{
+	int i, j;
+
+	for (i = 0; i < n - 1; i++) {
+		for (j = 0; j < n - i - 1; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	int i;
+	int height[NUMBER];
+
+	printf("请输入 %d 人的身高。\n", NUMBER);
+	for (i = 0; i < NUMBER; i++) {
+		printf("%2d 号：", i + 1);
+		scanf("%d", &height[i]);
+	}
+
+	bsort(height, NUMBER);
+
+	puts("按升序排列。");
+	for (i = 0; i < NUMBER; i++)
+		printf("%2d 号：%d\n", i + 1, height[i]);
+
+	return 0;
+}
