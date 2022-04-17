da29648c294c414a32a8aec4e553e885f956850a
trqii <3038605486@qq.com>
2022-04-17 21:27:04 +0800

    8-5

---
+++ 明解C语言入门篇第三版/chapter08/8-5.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+enum sex
+{
+	Male,
+	Famale,
+};
+
+enum season
+{
+	Spring,
+	Summer,
+	Autumn,
+	Winter
+};
+
+int main(void)
+{
+	enum sex s = Famale;
+
+	if (s == Male)
+		puts("it's Male.\n");
+	else
+		puts("it's Famale\n");
+
+	enum sex ss = Spring;
+
+	switch (ss)
+	{
+	case Spring: puts("it's spring."); break;
+	case Summer: puts("it's summer."); break;
+	case Autumn: puts("it's autumn."); break;
+	case Winter: puts("it's winter."); break;
+	}
+
+	return 0;
+}
