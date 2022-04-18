040b3a9d5941075010a0a246def9dd0b1b9dbbb7
trqii <3038605486@qq.com>
2022-04-18 22:34:45 +0800

    8-6

---
+++ 明解C语言入门篇第三版/chapter08/8-6.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int factorial(int n)
+{
+	int ret = 1;
+	int i;
+
+	for (i = 2; i <= n; i++)
+		ret *= i;
+
+	return ret;
+}
+
+int main(void)
+{
+	int num;
+
+	printf("请输入一个整数：");
+	scanf("%d", &num);
+
+	printf("%d 的阶乘为 %d。\n", num, factorial(num));
+
+	return 0;
+}
