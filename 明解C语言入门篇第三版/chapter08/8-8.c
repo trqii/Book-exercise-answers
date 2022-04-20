c18d078c483d60e1da5edbe5ee99c9e58c995195
trqii <3038605486@qq.com>
2022-04-20 12:23:24 +0800

    8-8

---
+++ 明解C语言入门篇第三版/chapter08/8-8.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int gcd(int x, int y)
+{
+	return y ? gcd(y, x % y) : x;
+}
+
+int main(void)
+{
+	int x, y;
+
+	printf("请输入一个整数x："); scanf("%d", &x);
+	printf("请输入一个整数y："); scanf("%d", &y);
+
+	printf("最大公约数为：%d。\n", gcd(x, y));
+
+	return 0;
+}
