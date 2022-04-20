f9cb3814feedf3e876bde9917056a6d85dcb97c8
trqii <3038605486@qq.com>
2022-04-21 00:02:36 +0800

    8-9

---
+++ 明解C语言入门篇第三版/chapter08/8-9.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int ch;
+	int count = 0;
+
+	while ((ch = getchar()) != EOF)
+		if (ch == '\n')
+			count++;
+
+	printf("一共输入了%d行。\n", count);
+
+	return 0;
+}
