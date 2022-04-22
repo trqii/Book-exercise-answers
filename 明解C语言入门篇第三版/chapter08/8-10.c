f51407d9fe5a9c8eb704591e647a04de821b2035
trqii <3038605486@qq.com>
2022-04-22 22:03:33 +0800

    8-10

---
+++ 明解C语言入门篇第三版/chapter08/8-10.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i, j, ch;
+	int cnt[10] = { 0 };
+
+	while ((ch = getchar()) != EOF) {
+		if (ch >= '0' && ch <= '9')
+			cnt[ch - '0']++;
+	}
+
+	puts("数字字符的出现次数");
+	for (i = 0; i < 10; i++) {
+		printf("'%d'：", i);
+		for (j = 0; j < cnt[i]; j++)
+			printf("*");
+		putchar('\n');
+	}
+	return 0;
+}
