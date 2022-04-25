271415c604041758ede84257fc924be47e3b5440
trqii <3038605486@qq.com>
2022-04-25 16:47:17 +0800

    9-3

---
+++ 明解C语言入门篇第三版/chapter09/9-3.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+#define NUMBER 10
+
+int is_sign(char s[])
+{
+	int i;
+	for (i = 0; i < 5; i++)
+		if (s[i] != '$')
+			return 0;
+	return 1;
+}
+
+int main(void)
+{
+	int i;
+	char s[NUMBER][128];
+
+	for (i = 0; i < NUMBER; i++) {
+		printf("s[%d]：", i);
+		scanf("%s", s[i]);
+		if (is_sign(s[i]))
+			break;
+	}
+	for (i = 0; i < NUMBER; i++) {
+		if (is_sign(s[i]))
+			break;
+		else
+			printf("s[%d] = \"%s\"\n", i, s[i]);
+	}
+
+	return 0;
+}
