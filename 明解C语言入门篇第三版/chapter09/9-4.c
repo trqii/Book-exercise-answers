84a1968270cee53f0d241fc02947ecd686574afd
trqii <3038605486@qq.com>
2022-04-26 22:00:37 +0800

    9-4

---
+++ 明解C语言入门篇第三版/chapter09/9-4.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+void null_string(char s[])
+{
+	int i = 0;
+	while (s[i])
+		s[i++] = '\0';
+}
+
+int main(void)
+{
+	char s[] = "abc";
+
+	printf("%s\n", s);
+
+	null_string(s);
+	printf("%s\n", s);
+
+	return 0;
+}
