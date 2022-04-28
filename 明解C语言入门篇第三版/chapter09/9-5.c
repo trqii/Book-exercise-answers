d332d7c9daef873d40ce2c31f70d7ea9ca46067b
trqii <3038605486@qq.com>
2022-04-28 22:48:20 +0800

    9-5

---
+++ 明解C语言入门篇第三版/chapter09/9-5.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int str_char(const char s[], int c)
+{
+	int count = -1;
+	int i = 0;
+
+	while (s[i])
+		if (s[i++] == c) {
+			count = i - 1;
+			break;
+		}
+
+	return count != -1 ? count : -1;
+}
+
+int main(void)
+{
+	char s[100];
+	char c;
+
+	printf("请输入一个字符串："); scanf("%s", s); getchar();
+	printf("请输入一个字符："); scanf("%c", &c);
+
+	printf("字符中 %s 中第一次出现字符 \'%c\'的位置是 %d 。\n", s, c, str_char(s, c));
+
+	return 0;
+}
