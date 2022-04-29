c2e4741bf122b36eee2445083e79bcfdeac26342
trqii <3038605486@qq.com>
2022-04-29 20:16:15 +0800

    9-6

---
+++ 明解C语言入门篇第三版/chapter09/9-6.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int str_chnum(const char s[], int c)
+{
+	int count = 0;
+	int i = 0;
+
+	while (s[i])
+		if (s[i++] == c)
+			count++;
+
+	return count;
+}
+
+int main(void)
+{
+	char s[100];
+	char c;
+
+	printf("请输入一个字符串："); scanf("%s", s); getchar();
+	printf("请输入一个字符："); scanf("%c", &c);
+
+	printf("字符中 %s 中出现字符 \'%c\' %d 次。\n", s, c, str_chnum(s, c));
+
+	return 0;
+}
