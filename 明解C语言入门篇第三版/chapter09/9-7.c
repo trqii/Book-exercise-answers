33cad276a982f1dec4d9dbddb53698e4c09eab7c
trqii <3038605486@qq.com>
2022-04-30 23:38:49 +0800

    9-7

---
+++ 明解C语言入门篇第三版/chapter09/9-7.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+void put_stringn(const char s[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		printf("%s", s);
+}
+
+int main(void)
+{
+	char s[100];
+	int n;
+
+	printf("请输入一个字符串："); scanf("%s", s); 
+	printf("请输入显示的次数："); scanf("%d", &n);
+
+	put_stringn(s, n);
+	printf("。\n");
+
+	return 0;
+}
