57c63d08a91278f9426279cf43aae0ff472c63e1
trqii <3038605486@qq.com>
2022-05-01 15:33:27 +0800

    9-8

---
+++ 明解C语言入门篇第三版/chapter09/9-8.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+void put_stringr(const char s[])
+{
+	int i = 0;
+	while (s[i])
+		i++;
+
+	while (--i >= 0)
+		putchar(s[i]);
+}
+
+int main(void)
+{
+	char s[100];
+
+	printf("请输入一个字符串："); scanf("%s", s);
+
+	put_stringr(s);
+
+	return 0;
+}
