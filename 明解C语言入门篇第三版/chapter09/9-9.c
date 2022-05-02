778df881ecac539b404f6aa088155522a045a207
trqii <3038605486@qq.com>
2022-05-02 20:29:18 +0800

    9-9

---
+++ 明解C语言入门篇第三版/chapter09/9-9.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+void rev_string(char s[])
+{
+	int i;
+	int len = 0;
+
+	while (s[len])
+		len++;
+
+	for (i = 0; i < len / 2; i++) {
+		char temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+
+	}
+}
+
+int main(void)
+{
+	char s[100];
+
+	printf("请输入一个字符串："); scanf("%s", s);
+
+	rev_string(s);
+
+	puts("反转后的字符串");
+	printf("%s\n", s);
+
+	return 0;
+}
