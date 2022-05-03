1ea6d95a70aaf2e5720c13b31266f93e792791ca
trqii <3038605486@qq.com>
2022-05-03 18:44:05 +0800

    9-10

---
+++ 明解C语言入门篇第三版/chapter09/9-10.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+void del_digit(char s[])
+{
+	int i = 0;
+	while (s[i]) {
+		if (s[i] < '0' || s[i] > '9')
+			putchar(s[i]);
+		i++;
+	}
+}
+
+int main(void)
+{
+	char s[100];
+
+	printf("请输入一个字符串："); scanf("%s", s);
+
+	puts("去除数字字符后");
+	del_digit(s);
+
+	return 0;
+}
