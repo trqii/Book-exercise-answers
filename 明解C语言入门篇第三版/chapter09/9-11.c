974dbb03075b34b1a8e71a4441157db4b6425dca
trqii <3038605486@qq.com>
2022-05-04 21:49:38 +0800

    9-11

---
+++ 明解C语言入门篇第三版/chapter09/9-11.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+
+#define NUMBER 10
+#define MAX 128
+
+void put_strary(const char s[][MAX], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		printf("s[%d] = \"%s\"\n", i, s[i]);
+}
+
+int is_sign(char s[])
+{
+	int i;
+	for (i = 0; i < 5; i++)
+		if (s[i] != '$')
+			return 0;
+	return 1;
+}
+
+int input(char s[][MAX], int n)
+{
+	int i;
+	for (i = 0; i < n; i++) {
+		scanf("%s", s[i]);
+		if (is_sign(s[i]))
+			return i;
+	}
+	return i;
+}
+
+int main(void)
+{
+	char cs[NUMBER][MAX];
+
+	int n = input(cs, NUMBER);
+
+	put_strary(cs, n);
+
+	return 0;
+}
