a4c3d6453c66f6a679f8098ec426f2c7a55cc434
trqii <3038605486@qq.com>
2022-05-07 00:06:17 +0800

    10-1

---
+++ 明解C语言入门篇第三版/chapter10/10-1.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+void adjust_point(int* n)
+{
+	if (*n < 0)
+		*n = 0;
+	else if (*n > 100)
+		*n = 100;
+}
+
+int main(void)
+{
+	int n;
+
+	printf("请输入一个整数："); scanf("%d", &n);
+
+	adjust_point(&n);
+
+	printf("%d\n", n);
+
+	return 0;
+}
