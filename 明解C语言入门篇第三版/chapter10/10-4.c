c3db6d6092f5d61b3b2236c9b649e58e2babf715
trqii <3038605486@qq.com>
2022-05-10 22:19:59 +0800

    10-4

---
+++ 明解C语言入门篇第三版/chapter10/10-4.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+void set_idx(int* v, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		v[i] = i;
+}
+
+int main(void)
+{
+	int v[5];
+	int i;
+
+	set_idx(v, 5);
+
+	puts("赋上和下标相同的值");
+	for (i = 0; i < 5; i++)
+		printf("v[%d] = %d\n", i, v[i]);
+
+	return 0;
+}
