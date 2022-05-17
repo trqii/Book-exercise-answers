9626c806c6e1c6f53f56021a8b22145f1d6c8f66
trqii <3038605486@qq.com>
2022-05-17 15:21:35 +0800

    11-4

---
+++ 明解C语言入门篇第三版/chapter11/11-4.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+void put_string(const char* s)
+{
+	while (*s)
+		putchar(*s++);
+}
+
+int main(void)
+{
+	char s[128];
+
+	printf("请输入一个字符串："); scanf("%s", s);
+
+	puts("你输入的是。");
+	put_string(s);
+	putchar('\n');
+
+	return 0;
+}
