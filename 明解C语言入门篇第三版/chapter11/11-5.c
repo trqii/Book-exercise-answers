b638d54e98c2059992d7284a34738d43e0e1c259
trqii <3038605486@qq.com>
2022-05-18 15:06:03 +0800

    11-5

---
+++ 明解C语言入门篇第三版/chapter11/11-5.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int str_chnum(const char* s, int c)
+{
+	int count = 0;
+
+	while (*s)
+		if (*s++ == c)
+			count++;
+
+	return count;
+}
+
+int main(void)
+{
+	char s[128];
+	char c;
+
+	printf("请输入一个字符串："); scanf("%s", s); getchar();
+	printf("请输入一个字符："); scanf("%c", &c);
+
+	printf("在字符串\"%s\"中出现了字符'%c'%d次。\n", s, c, str_chnum(s, c));
+
+	return 0;
+}
