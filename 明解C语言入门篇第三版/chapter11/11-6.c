aed79bb6d2bdcc9cd76d108e29ebbafaaa615aba
trqii <3038605486@qq.com>
2022-05-19 21:09:35 +0800

    11-6

---
+++ 明解C语言入门篇第三版/chapter11/11-6.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+char* str_chr(const char* s, int c)
+{
+	while (*s)
+		if (*s++ == c)
+			return --s;
+
+	return NULL;
+}
+
+int main(void)
+{
+	char s[128];
+	char c;
+
+	printf("请输入一个字符串："); scanf("%s", s); getchar();
+	printf("请输入一个字符："); scanf("%c", &c);
+
+	printf("在字符串\"%s\"中第一次出现了字符'%c'的位置 %s。\n", s, c, str_chr(s, c));
+
+	return 0;
+}
