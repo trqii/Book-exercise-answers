4e5a7ae5b643e5b57365db39ef32e8cb571e3b48
trqii <3038605486@qq.com>
2022-05-20 16:09:21 +0800

    11-7

---
+++ 明解C语言入门篇第三版/chapter11/11-7.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+void str_toupper(char* s)
+{
+	while (*s)
+		*s++ = toupper(*s);
+}
+
+void str_tolower(char* s)
+{
+	while (*s)
+		*s++ = tolower(*s);
+}
+
+int main(void)
+{
+	char str[128];
+
+	printf("请输入字符串：");
+	scanf("%s", str);
+
+	str_toupper(str);
+	printf("大写字母：%s\n", str);
+
+	str_tolower(str);
+	printf("小写字母：%s\n", str);
+
+	return 0;
+}
