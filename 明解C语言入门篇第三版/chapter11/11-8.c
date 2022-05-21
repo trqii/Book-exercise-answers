9e1aead11e9e7a4087e036b4147eba1f7085a0b6
trqii <3038605486@qq.com>
2022-05-21 14:14:58 +0800

    11-8

---
+++ 明解C语言入门篇第三版/chapter11/11-8.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+void del_digit(char* str)
+{
+	while (*str) {
+		if (!isdigit(*str))
+			putchar(*str);
+		str++;
+	}
+}
+
+int main(void)
+{
+	char str[128];
+
+	printf("请输入字符串：");
+	scanf("%s", str);
+
+	puts("删除数字字符。");
+	del_digit(str);
+
+	return 0;
+}
