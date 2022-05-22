65ec5131a4f1710a104ebb1fc6e68ef69ccef4a9
trqii <3038605486@qq.com>
2022-05-22 13:56:01 +0800

    11-9

---
+++ 明解C语言入门篇第三版/chapter11/11-9.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str[128];
+
+	while (1) {
+		printf("请输入密码："); scanf("%s", str);
+		if (strlen(str) < 8) {
+			puts("密码应该大于等于8位\n");
+			continue;
+		}
+
+		if (strcmp(str, "12345678") == 0) {
+			puts("密码正确");
+			break;
+		}
+		else {
+			puts("密码错误");
+		}
+	}
+
+	return 0;
+}
