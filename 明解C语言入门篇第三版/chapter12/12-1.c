25c6dd55cf0643cb11a323564e32f68ea305cef4
trqii <3038605486@qq.com>
2022-05-25 21:17:25 +0800

    12-1

---
+++ 明解C语言入门篇第三版/chapter12/12-1.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+#define NAME_LEN 64
+
+struct student {
+	char name[NAME_LEN];
+	int height;
+	float weight;
+	long schols;
+};
+
+int main(void)
+{
+	struct student takao = { "Takao", 173, 86.2 };
+
+	printf("姓名的地址 = %p\n", takao.name);
+	printf("身高的地址 = %p\n", &takao.height);
+	printf("体重的地址 = %p\n", &takao.weight);
+	printf("奖学金的地址 = %p\n", &takao.schols);
+
+	return 0;
+}
