e889ad56eabe4c39b308b24cdc059dfd6b093f79
trqii <3038605486@qq.com>
2022-05-26 21:01:28 +0800

    12-2

---
+++ 明解C语言入门篇第三版/chapter12/12-2.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+#define NAME_LEN 64
+
+typedef struct student {
+	char name[NAME_LEN];
+	int height;
+	float weight;
+	long schols;
+}Student;
+
+void hiroko(Student* std)
+{
+	if (std->height < 180) std->height = 180;
+	if (std->weight > 80) std->weight = 80;
+}
+
+int main(void)
+{
+	struct student sanaka;
+
+	printf("请输入姓名：");   scanf("%s", sanaka.name);
+	printf("请输入身高：");   scanf("%d", &sanaka.height);
+	printf("请输入体重：");   scanf("%f", &sanaka.weight);
+	printf("请输入奖学金："); scanf("%ld", &sanaka.schols);
+
+	hiroko(&sanaka);
+
+	printf("姓名 = %s\n", sanaka.name);
+	printf("身高 = %d\n", sanaka.height);
+	printf("体重 = %.1f\n", sanaka.weight);
+	printf("奖学金 = %ld\n", sanaka.schols);
+
+	return 0;
+}
