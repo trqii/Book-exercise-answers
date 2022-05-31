a4ce1706deccb4cdee55247db6c04e53e398e687
trqii <3038605486@qq.com>
2022-05-31 21:46:06 +0800

    13-1

---
+++ 明解C语言入门篇第三版/chapter13/13-1.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp;
+	char arr[128];
+
+	printf("请输入文件名 : "); scanf("%s", arr);
+
+	fp = fopen(arr, "r");
+
+	if (fp == NULL)
+		puts("该文件不存在。");
+	else {
+		puts("该文件存在。");
+		fclose(fp);
+	}
+
+	return 0;
+}
