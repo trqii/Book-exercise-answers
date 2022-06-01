2081a093b92921137250511cc8e24ff3718061ee
trqii <3038605486@qq.com>
2022-06-01 22:45:18 +0800

    13-2

---
+++ 明解C语言入门篇第三版/chapter13/13-2.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp;
+	char arr[128];
+
+	printf("请输入文件名 : "); scanf("%s", arr);
+
+	fp = fopen(arr, "w");
+
+	puts("文件内容以消除。");
+
+	fclose(fp);
+
+	return 0;
+}
