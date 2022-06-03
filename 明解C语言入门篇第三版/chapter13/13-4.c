20da03853f507c0191a52892c02de6c600e7488e
trqii <3038605486@qq.com>
2022-06-03 21:37:20 +0800

    13-4

---
+++ 明解C语言入门篇第三版/chapter13/13-4.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+struct stu {
+	char name[100];
+	double height;
+	double weight;
+};
+
+int main(void)
+{
+	FILE* fp;
+	struct stu s[100];
+	int i = 0;
+
+	if ((fp = fopen("hw.dat", "w")) == NULL)
+		printf("\a文件打开失败。\n");
+	else {
+		puts("姓名  身高  体重");
+		while (scanf("%s%lf%lf", s[i].name, &s[i].height, &s[i].weight) == 3) {
+			fprintf(fp, "%-10s %5.1f %5.1f\n", s[i].name, s[i].height, s[i].weight);
+			i++;
+		}
+		fclose(fp);
+	}
+
+	return 0;
+}
