87d6617dc444300fd0ab4cce8aed01825ab05bdc
trqii <3038605486@qq.com>
2022-06-05 18:19:15 +0800

    13-6

---
+++ 明解C语言入门篇第三版/chapter13/13-6.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int ch;
+	int count = 0;
+	FILE* fp;
+	char fname[FILENAME_MAX];
+
+	printf("请输入文件名 : "); scanf("%s", fname);
+
+	if ((fp = fopen(fname, "r")) == NULL)
+		printf("\a文件打开失败。\n");
+	else {
+		while ((ch = fgetc(fp)) != EOF)
+			if (ch == '\n')
+				count++;
+
+		printf("文件一共%d行。\n", count);
+		fclose(fp);
+	}
+
+	return 0;
+}
