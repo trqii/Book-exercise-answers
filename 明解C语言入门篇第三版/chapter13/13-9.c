388ae1b24d4e3bce6d0c67703d06aee644b5e4d5
trqii <3038605486@qq.com>
2022-06-08 17:44:16 +0800

    13-9

---
+++ 明解C语言入门篇第三版/chapter13/13-9.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	int ch;
+	FILE* sfp;
+	FILE* dfp;
+	char sname[FILENAME_MAX];
+	char dname[FILENAME_MAX];
+
+	printf("打开原文件: ");   scanf("%s", sname);
+	printf("打开目标文件: "); scanf("%s", dname);
+
+	if ((sfp = fopen(sname, "r")) == NULL)
+		printf("\a原文件打开失败。\n");
+	else {
+		if ((dfp = fopen(dname, "w")) == NULL)
+			printf("\a目标文件打开失败。\n");
+		else {
+			while ((ch = fgetc(sfp)) != EOF) {
+				ch = toupper(ch);
+				fputc(ch, stdout);
+				fputc(ch, dfp);
+			}
+		}
+	}
+
+	return 0;
+}
