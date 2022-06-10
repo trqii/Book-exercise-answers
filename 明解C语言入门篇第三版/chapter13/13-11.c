26670e1682f6023351af6729a4d9f1f971827190
trqii <3038605486@qq.com>
2022-06-10 21:01:55 +0800

    13-11

---
+++ 明解C语言入门篇第三版/chapter13/13-11.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	double arr[10];
+	int i;
+	FILE* fp = fopen("hw.dat", "rb");
+
+	if (fp == NULL)
+		printf("\a文件打开失败。\n");
+	else {
+		fread(arr, sizeof(double), 10, fp);
+
+		for (i = 0; i < 10; i++)
+			printf("%f\n", arr[i]);
+
+		fclose(fp);
+	}
+
+	return 0;
+}
